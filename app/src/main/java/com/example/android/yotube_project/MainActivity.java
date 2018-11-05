5886188264841656f0de74d9723223c19bf1ff0e
pedrohjc <pedrohj.carvalho@gmail.com>
2018-11-05 16:02:46 -0500

    First Commit

---
+++ app/src/main/java/com/example/android/yotube_project/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.android.yotube_project;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> shoppingList = null;
+    ArrayAdapter<String> adapter = null;
+    ListView lv = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        shoppingList = new ArrayList<>();
+//        Collections.addAll(shoppingList, "Eggs", "Yogurt", "Milk", "Bananas", "Apples", "Tide with bleach", "Cascade");
+//        shoppingList.addAll(Arrays.asList("Napkins", "Dog food", "Chapstick", "Bread"));
+//        shoppingList.add("Sunscreen");
+//        shoppingList.add("Toothpaste");
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingList);
+        lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_sort) {
+            Collections.sort(shoppingList);
+            lv.setAdapter(adapter);
+            return true;
+        }
+
+        if (id == R.id.action_add){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Add Item");
+            final EditText input = new EditText(this);
+            builder.setView(input);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    shoppingList.add(preferredCase(input.getText().toString()));
+                    Collections.sort(shoppingList);
+                    lv.setAdapter(adapter);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            return true;
+        }
+
+        if (id == R.id.action_clear){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Do you really want to clear the list?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    shoppingList.clear();
+                    lv.setAdapter(adapter);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            lv.setAdapter(adapter);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static String preferredCase(String original)
+    {
+        if (original.isEmpty())
+            return original;
+
+        return original.substring(0, 1).toUpperCase() + original.substring(1).toLowerCase();
+    }
+
+    public static void storeArrayVal( ArrayList<String> inArrayList, Context context)
+    {
+        Set<String> WhatToWrite = new HashSet<String>(inArrayList);
+        SharedPreferences WordSearchPutPrefs = context.getSharedPreferences("dbArrayValues", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = WordSearchPutPrefs.edit();
+        prefEditor.putStringSet("myArray", WhatToWrite);
+        prefEditor.commit();
+    }
+
+    public static ArrayList getArrayVal( Context dan)
+    {
+        SharedPreferences WordSearchGetPrefs = dan.getSharedPreferences("dbArrayValues",Activity.MODE_PRIVATE);
+        Set<String> tempSet = new HashSet<String>();
+        tempSet = WordSearchGetPrefs.getStringSet("myArray", tempSet);
+        return new ArrayList<String>(tempSet);
+    }
+}
